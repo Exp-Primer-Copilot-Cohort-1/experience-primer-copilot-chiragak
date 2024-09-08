30acf5129b16127dc1691efed66206e43a19347f
Chirag A K <70628014+chiragak@users.noreply.github.com>
2024-09-08 19:53:10 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,18 @@
+//Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//Create routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/comments', (req, res) => {
+  res.send('This is the comments page');
+});
+
+//Start web server
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
